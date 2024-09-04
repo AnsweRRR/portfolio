25f3449e285e705f1b249bb8d3c22c8651ea303d
Tamás Pogrányi <tamas.pogranyi@gsgroup.hu>
2024-09-04 12:55:01 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
+
+import Hero from './components/Hero'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Hero />
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Hero.tsx
@@ -0,0 +1,19 @@
+import { Box } from "@mui/material";
+import ComputersCanvas from "./canvas/Computer";
+
+const Hero = () => {
+	return (
+		<Box
+			teszt={{}}
+			component="section"
+		>
+			<Box
+				
+			>
+				<ComputersCanvas />
+			</Box>
+		</Box>
+	);
+}
+
+export default Hero;
