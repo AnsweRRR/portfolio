d9473bd407aa864dd78f04b018601d60d91371fc
Tamás Pogrányi <pogranyitamas99@gmail.com>
2024-09-14 17:31:28 +0200

    change everything get rid of mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
 import { BrowserRouter } from "react-router-dom";
-import Hero from './components/Hero'
-import ThemeProvider from "./components/theme";
-import { Box, styled } from "@mui/material";
-
-const AppContainer = styled(Box)(({ theme }) => ({
-	position: 'relative',
-  zIndex: 0,
-  // TODO: backgroundColor: theme.palette.primary.main
-  backgroundColor: '#050816'
-})) as typeof Box;
-
-const HeroContainer = styled(Box)(() => ({
-  backgroundSize: 'cover',
-  backgroundRepeat: 'no-repeat',
-  backgroundPosition: 'center',
-})) as typeof Box;
+import Hero from "./components/Hero";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <ThemeProvider>
-          <AppContainer>
-            <HeroContainer>
-              <Hero />
-            </HeroContainer>
-          </AppContainer>
-          
-        </ThemeProvider>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          {/* <Navbar /> */}
+          <Hero />
+        </div>
+        {/* <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div> */}
+      </div>
+    </BrowserRouter>
   )
 }
 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,43 +1,50 @@
-import { Box, styled } from "@mui/material";
+import { styles } from "../utils/styles";
 import ComputersCanvas from "./canvas/Computer";
+import { motion } from "framer-motion";
 
-const HeroSection = styled(Box)(() => ({
-	position: 'relative',
-	width: '100%',
-	height: '100vh',
-	marginLeft: 'auto',
-	marginRight: 'auto'
-})) as typeof Box;
+const Hero = () => {
+  return (
+    <section className={`relative w-full h-screen mx-auto`}>
+      <div
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        </div>
 
-const CanvasContainer = styled(Box)(({ theme }) => ({
-	position: 'absolute',
-	top: '120px',
-	right: 0,
-	bottom: 0,
-	left: 0,
-	maxWidth: '80rem',
-	marginLeft: 'auto',
-	marginRight: 'auto',
-	display: 'flex',
-	flexDirection: 'row',
-	alignItems: 'flex-start',
-	gap: '1.25rem',
-	paddingLeft: '1.5rem',
-	paddingRight: '1.5rem',
-	[theme.breakpoints.up('sm')]: {
-		paddingLeft: '4rem',
-		paddingRight: '4rem',
-	},
-})) as typeof Box;
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className='text-[#915EFF]'>Tamás</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I'm a full-stack developer <br className='sm:block hidden' />
+            mainly /w React & .NET
+          </p>
+        </div>
+      </div>
 
-const Hero: React.FC = () => {
-	return (
-		<HeroSection component="section">
-			<CanvasContainer>
-				<ComputersCanvas />
-			</CanvasContainer>
-		</HeroSection>
-	);
-}
+      <ComputersCanvas />
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+};
 
 export default Hero;
