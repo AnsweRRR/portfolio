9e1863dd43dc722671fdd2df37243e73bbe67a00
Tamás Pogrányi <pogranyitamas99@gmail.com>
2025-05-21 18:02:45 +0200

    fix computercanvas on small screen

--- src/components/canvas/Computer.tsx
+++ src/components/canvas/Computer.tsx
@@ -24,8 +24,8 @@ const Computers: React.FC<ComputersProps> = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.6 : 0.75}
+        position={isMobile ? [-2, -3.5, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
