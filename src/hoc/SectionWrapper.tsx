988fe91f9e862f2aa8b6836e81cec268566f8b3d
Tamás Pogrányi <pogranyitamas99@gmail.com>
2026-03-22 16:28:33 +0100

    fix: update viewport amount in SectionWrapper for improved responsiveness

--- src/hoc/SectionWrapper.tsx
+++ src/hoc/SectionWrapper.tsx
@@ -9,7 +9,7 @@ const SectionWrapper = (Component: React.ComponentType, idName: string) =>
         variants={staggerContainer()}
         initial='hidden'
         whileInView='show'
-        viewport={{ once: true, amount: 0.25 }}
+        viewport={{ once: true, amount: "some" }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
       >
         <span className='hash-span' id={idName}>
